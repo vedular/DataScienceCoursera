e8defa899b26d6f3aab9dce6756ca726b2861514
vedular <34138126+vedular@users.noreply.github.com>
2017-12-03 21:40:30 -0600

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,43 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  # Define function to set the value of the matrix. It also clears the old
+  # inverse from the cache
+  set <- function(y) {
+    x <<- y    # Set the value
+    m <<- NULL # Clear the cache
+  }
+  # Define function to get the value of the matrix
+  get <- function() x
+  # Define function to set the inverse. This is only used by getinverse() when
+  # there is no cached inverse
+  setInverse <- function(inverse) m <<- inverse
+  # Define function to get the inverse
+  getInverse <- function() m
+  
+  # Return a list with the above four functions
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+#' Return inverse of matrix x
+#' 
+
+#' 
+#' @param x a special matrix created with makeCacheMatrix
+#' 
+#' @return The inverse of the matrix x
+#' 
+cacheSolve <- function(x) {
+  m <- x$getInverse() # This fetches the cached value for the inverse
+  if(!is.null(m)) { # If the cache was not empty, we can just return it
+    message("getting cached data")
+    return(m)
+  }
+  # The cache was empty. We need to calculate it, cache it, and then return it.
+  data <- x$get()  # Get value of matrix
+  m <- solve(data) # Calculate inverse
+  x$setInverse(m)  # Cache the result
+  m                # Return the inverse
+}
